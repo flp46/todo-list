fbc88c67ce8529bca785e477a9cc6b91d5358653
flp46 <alarconnieto@gmail.com>
2021-10-25 16:54:18 -0500

    Creacion componentes basicos + elimino cosas sin usar + ajustes de UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoItems } from './TodoItems';
+import { TodoList } from './TodoList';
+import { CreateButtom } from './CreateButtom';
+
+const todos = [
+  {text: "Hola1", completed: false},
+  {text: "Hola2", completed: false},
+  {text: "Hola3", completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and let the magic rise
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      {/* <h2>Has completado 2 de 3 cursos </h2> */}
+      <TodoSearch />
+      {/* <input placeholder="Cebolla" /> */}
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItems key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateButtom />
+      {/* <button>+</button> */}
+    </React.Fragment>
   );
 }
 
---
+++ src/CreateButtom.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CreateButtom(){
+    return(
+        <button>+</button>
+    )
+}
+
+export { CreateButtom };
